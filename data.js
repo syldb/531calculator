a0e405620ff5a1a210878ae83787f9bd5922b94b
Sylvain Delabye <sylvain.delabye@people-doc.com>
2019-07-19 11:01:07 +0200

    separate data in a file

---
+++ data.js
@@ -0,0 +1,22 @@
+var referenceTableMainLifts = [
+  [65, 75, 85],
+  [70, 80, 90],
+  [75, 85, 95],
+  [40, 50, 60]
+];
+var referenceTableAssitanceLifts = [
+  [50, 60, 70],
+  [60, 70, 80],
+  [65, 75, 85],
+  [40, 50, 60]
+];
+var isMainLiftReference = {
+  "ohp": true,
+  "cg-bench": false,
+  "deadlift": true,
+  "front-squat": false,
+  "bench": true,
+  "incline-bench": false,
+  "squat": true,
+  "sl-deadlift": false
+}
